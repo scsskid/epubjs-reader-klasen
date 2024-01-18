9fb567ee884f87b94e878c0ba21611fbedc82012
Benedikt Samuelsen <benedikt.samuelsen@sprylab.com>
2024-01-18 10:19:52 +0100

    improve sizings, save settings

--- src/panels/FontSizeSlider.js
+++ src/panels/FontSizeSlider.js
@@ -6,36 +6,44 @@ export class FontSizeSlider {
 		this.listen();
 		this.init();
 
+		console.log(reader.settings.styles.fontSize);
+		this.input.value = reader.settings?.styles?.fontSize || 100;
+
 		// !throws webpack error
 		// reader.on("bookready", (e) => {
 		//   console.log("bookready -> init()", e);
+		//   this.init();
 		// }
 	}
 
+	// create Dom elements for font size widget
 	buildDomElements() {
 		this.container = document.createElement("div");
-		this.container.style["position"] = "fixed";
-		this.container.style["zIndex"] = "100";
-		this.container.textContent = "Font Size";
 		this.container.classList.add("font-size-slider");
+		this.container.textContent = "Font Size";
 
 		this.input = document.createElement("input");
 		this.input.type = "range";
+		this.input.min = 90;
+		this.input.max = 150;
+		this.input.step = 10;
 
 		this.container.appendChild(this.input);
 	}
 
+	// listen for changes to font size widget
+	// emit styleschanged event to apply new font size
+	// save settings to local storage
 	listen() {
 		this.input.addEventListener("input", (e) => {
-			console.log(e.target.value);
 			reader.emit("styleschanged", {
-				fontSize: parseInt(e.target.value) * 2,
+				fontSize: parseInt(e.target.value),
 			});
+			reader.saveSettings();
 		});
 	}
 
 	init() {
-		console.log("FontSizeSlider2 init");
 		document.body.append(this.container);
 	}
 }
